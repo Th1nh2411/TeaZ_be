727b49dd25c15f586d95b8dd4c0da2a0e3465efa
Thinh Nguyen <rinktvn2525@gmail.com>
2023-08-14 23:12:03 +0700

    refactor code

--- controllers/order.controllers.js
+++ controllers/order.controllers.js
@@ -804,6 +804,79 @@ const searchRecipe = async (req, res) => {
         res.status(500).json({ isSuccess: false });
     }
 };
+const create_payment_url = async (req, res) => {
+    var ipAddr =
+        req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+
+    var config = require('../config/vnpay_config.json');
+
+    var tmnCode = config.vnp_TmnCode;
+    var secretKey = config.vnp_HashSecret;
+    var vnpUrl = config.vnp_Url;
+    var returnUrl = config.vnp_ReturnUrl;
+
+    var date = new Date();
+
+    let createDate = moment(date).format('YYYYMMDDHHmmss');
+    let orderId = req.body.orderId;
+    var amount = req.body.amount;
+    var bankCode = req.body.bankCode;
+    var locale = req.body.language;
+    if (locale === null || locale === '' || locale === undefined) {
+        locale = 'vn';
+    }
+    var currCode = 'VND';
+    var vnp_Params = {};
+    vnp_Params['vnp_Version'] = '2.1.0';
+    vnp_Params['vnp_Command'] = 'pay';
+    vnp_Params['vnp_TmnCode'] = tmnCode;
+    // vnp_Params['vnp_Merchant'] = ''
+    vnp_Params['vnp_Locale'] = locale;
+    vnp_Params['vnp_CurrCode'] = currCode;
+    vnp_Params['vnp_TxnRef'] = orderId;
+    vnp_Params['vnp_OrderInfo'] = 'Thanh toan cho ma GD:' + orderId;
+    vnp_Params['vnp_OrderType'] = 'other';
+    vnp_Params['vnp_Amount'] = amount * 100;
+    vnp_Params['vnp_ReturnUrl'] = returnUrl;
+    vnp_Params['vnp_IpAddr'] = ipAddr;
+    vnp_Params['vnp_CreateDate'] = createDate;
+    if (bankCode !== null && bankCode !== '' && bankCode !== undefined) {
+        vnp_Params['vnp_BankCode'] = bankCode;
+    }
+
+    vnp_Params = sortObject(vnp_Params);
+
+    var querystring = require('qs');
+    var signData = querystring.stringify(vnp_Params, { encode: false });
+    var crypto = require('crypto');
+    var hmac = crypto.createHmac('sha512', secretKey);
+    var signed = hmac.update(new Buffer(signData, 'utf-8')).digest('hex');
+    vnp_Params['vnp_SecureHash'] = signed;
+    vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+
+    // res.redirect(vnpUrl);
+    // res.render('success', { url: vnpUrl });
+    return res.status(200).json({ isSuccess: true, url: vnpUrl });
+};
+
+function sortObject(obj) {
+    let sorted = {};
+    let str = [];
+    let key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            str.push(encodeURIComponent(key));
+        }
+    }
+    str.sort();
+    for (key = 0; key < str.length; key++) {
+        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, '+');
+    }
+    return sorted;
+}
 module.exports = {
     // getDetailTaiKhoan,
     getToppingOptions,
@@ -826,4 +899,5 @@ module.exports = {
     getInvoiceProduct,
     searchRecipe,
     changeIngredientByCart,
+    create_payment_url,
 };
